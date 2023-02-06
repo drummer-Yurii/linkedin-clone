bd0e844257f411ad0999bc5389f0c473df37ff1b
Yurii Rybachok <drummer2009@ukr.net>
2023-02-06 11:15:18 +0200

    add leftside component to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import Leftside from "./Leftside";
+import Main from "./Main";
+import Rightside from "./Rightside";
 
 const Home = (props) => {
     return (
@@ -8,9 +11,9 @@ const Home = (props) => {
                 <p>Find talented pros in record time with Upwork and keep business moving.</p>
             </Section>
             <Layout>
-                <div>Left Side</div>
-                <div>Main</div>
-                <div>Right Side</div>
+                <Leftside />
+                <Main />
+                <Rightside />
             </Layout>
         </Container>
     )
