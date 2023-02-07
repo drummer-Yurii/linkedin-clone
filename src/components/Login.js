51865af208677aa8a8602a35b47eda5cd7b93670
Yurii Rybachok <drummer2009@ukr.net>
2023-02-07 17:50:02 +0200

    add login functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { connect } from "react-redux";
+import { signInAPI } from "../actions";
 
 const Login = (props) => {
     return (
@@ -19,7 +20,7 @@ const Login = (props) => {
                     <img src="/images/login-hero.svg" alt="" />
                 </Hero>
                 <Form>
-                    <Google>
+                    <Google onClick={() => props.SignIn()}>
                         <img src="/images/google.svg" alt="" />
                         Sign in with Google
                     </Google>
@@ -171,6 +172,8 @@ const mapStateToProps = (state) => {
     return {};
 };
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+    SignIn: () => dispatch(signInAPI()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
